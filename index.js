5ed1474fbc2f167cd4bee6482683cc8858803846
CryptoKiddd <avaliani1232@gmail.com>
2024-03-24 19:20:05 +0400

    boulevard init, nav responsive till 900px

---
+++ index.js
@@ -0,0 +1,18 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat:41.64847996800162, lng: 41.64134453062122},
+    zoom: 17,
+  });
+
+  new google.maps.Marker({
+    position:{ lat: 41.6475, lng: 41.6453},
+    map:map,
+    label:"Cable Car",
+    icon:"logonotext.png"
+
+  })
+}
+
+window.initMap = initMap;
