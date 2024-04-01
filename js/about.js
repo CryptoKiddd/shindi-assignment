db9ca3ef865c3ea1bf1836f0c75f71ba8e39e729
CryptoKiddd <avaliani1232@gmail.com>
2024-04-01 04:41:29 +0400

    1

--- js/about.js
+++ js/about.js
@@ -19,12 +19,95 @@ function slideshow(){
 }
 slideshow()
 next.onclick=function(){
+    
     active = active +1 < slides.length ? active + 1:active;
+    if(active === 0){
+       
+        about.style.background='#224862'
+        about.style.color='white'
+        ambitions.style.background='white'
+        goals.style.background='white'
+    
+        goals.style.color='#224862'
+        ambitions.style.color='#224862'
+    
+        header.textContent = 'ჩვენს შესახებ'
+        subheader.textContent = 'ჩვენს შესახებ'
+    }else if(active === 1){
+        
+        ambitions.style.background='#224862'
+        ambitions.style.color='white'
+    
+        goals.style.background='white'
+        about.style.background="white"
+    
+        goals.style.color='#224862'
+        about.style.color='#224862'
+        header.textContent = 'კომპანიის ხედვები და ამოცანები'
+        subheader.textContent = 'კომპანიის ხედვები და ამოცანები'
+    }
+    else if(active === 2){
+        
+        goals.style.background='#224862'
+        goals.style.color='white'
+        
+        about.style.background="white"
+        ambitions.style.background='white'
+    
+        about.style.color='#224862'
+        ambitions.style.color='#224862'
+        header.textContent = 'სამომავლო მიზნები'
+        subheader.textContent = 'სამომავლო მიზნები'
+    
+    }
+
+
     slideshow()
 }
 prev.onclick=function(){
  
     active = active -1 >= 0 ? active - 1:active;
+   
+    if(active === 0){
+       
+        about.style.background='#224862'
+        about.style.color='white'
+        ambitions.style.background='white'
+        goals.style.background='white'
+    
+        goals.style.color='#224862'
+        ambitions.style.color='#224862'
+    
+        header.textContent = 'ჩვენს შესახებ'
+        subheader.textContent = 'ჩვენს შესახებ'
+    }else if(active === 1){
+        
+        ambitions.style.background='#224862'
+        ambitions.style.color='white'
+    
+        goals.style.background='white'
+        about.style.background="white"
+    
+        goals.style.color='#224862'
+        about.style.color='#224862'
+        header.textContent = 'კომპანიის ხედვები და ამოცანები'
+        subheader.textContent = 'კომპანიის ხედვები და ამოცანები'
+    }
+    else if(active === 2){
+        
+        goals.style.background='#224862'
+        goals.style.color='white'
+        
+        about.style.background="white"
+        ambitions.style.background='white'
+    
+        about.style.color='#224862'
+        ambitions.style.color='#224862'
+        header.textContent = 'სამომავლო მიზნები'
+        subheader.textContent = 'სამომავლო მიზნები'
+    
+    }
+
     slideshow()
 }
 about.onclick=function(){
